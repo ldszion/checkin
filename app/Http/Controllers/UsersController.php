222a334eb78c413a370574d5b06015e33e637d63
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-02-04 20:38:42 -0200

    [modify] controller UsersController para retornar usuario com ala
    O controller foi alterado para retornar os usuarios com suas
    alas e estacas.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\User;
 use App\Http\Controllers\Controller;
+use Hash;
 
 class UsersController extends Controller
 {
@@ -17,28 +18,60 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return User::all()->toArray();
+        return User::with('ward.stake')->get();
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Inere um novo usuario
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  string                    $id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function insert(Request $request, $id = null)
     {
-        //
+        try {
+            if (!$request->has('password')) {
+                return response('PASSWORD_REQUIRED', 400);
+            }
+            $user = new User($request->all());
+            $user->password = Hash::make($request->password);
+
+            if ($user->save()) {
+                return $user->fresh()->with('ward.stake')->findOrFail($user->id);
+            }
+            return response('USER_NOT_SAVED', 400);
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Atualiza um usuario
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string                    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id = null)
+    {
+        $user = null;
+        try {
+            $user = User::findOrFail($id);
+        } catch (\Exception $e) {
+            return response('USER_NOT_FOUND', 500);
+        }
+        try {
+            $user->fill($request->all());
+            if ($request->has('password')) {
+                $user->password = Hash::make($request->password);
+            }
+            if ($user->save()) {
+                return $user->with('ward.stake')->findOrFail($user->id);
+            }
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 400);
+        }
     }
 
     /**
@@ -49,30 +82,7 @@ class UsersController extends Controller
      */
     public function show($id)
     {
-        return User::findOrFail($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        return User::with('ward.stake')->findOrFail($id);
     }
 
     /**
