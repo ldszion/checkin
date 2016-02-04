dfd256b675726d7199d154bfed4882afce7d1961
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-02-04 20:20:49 -0200

    [add] campos fake para usuarios em ModelFactory
    Foram adicionados os campos fake para usuarios na factory de usuarios

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,9 +12,15 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
+    $gender = ['male', 'female'];
+    $rand = rand(0, 1);
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'gender' => $gender[$rand],
+        'birthday' => $faker->dateTimeThisCentury->format('Y-m-d\TH:i:s.uO'),
         'password' => bcrypt('123456'),
         'remember_token' => str_random(10),
     ];
