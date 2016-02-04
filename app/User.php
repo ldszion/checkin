565bd74d565ba9e0fa3191661f6966cfa15d4184
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-02-04 20:46:13 -0200

    [modify] App\User para adicionar relacionamento de ward e atributos
    Foram adicionados os atributos permitidos para escrita e também o
    relacionamento com ala (ward);

--- app/User.php
+++ app/User.php
@@ -2,17 +2,19 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
-class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
     use Authenticatable, Authorizable, CanResetPassword;
 
@@ -28,7 +30,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'address', 'phone', 'gender', 'birthday', 'ward_id'];
 
     /**
      * The attributes excluded from the model's JSON form.
@@ -36,4 +38,29 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'birthday'];
+
+    /**
+     * Relacionamento com Ward
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function ward()
+    {
+        return $this->belongsTo('App\Ward');
+    }
+
+    public function setBirthdayAttribute($date)
+    {
+        if ($date) {
+            $date = Carbon::createFromFormat('Y-m-d\TH:i:s.uO', $date);
+            $this->attributes['birthday'] = $date;
+        }
+    }
 }
